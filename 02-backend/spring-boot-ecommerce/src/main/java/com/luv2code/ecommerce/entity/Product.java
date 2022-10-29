2fc933a83a4713379f8eb8b36de07a2992e4d759
ahmdalgebali <ahmedelgebalywork@gmail.com>
2022-10-29 20:27:35 +0200

    Checkout Form - Validation - Save the Order to the modified Database closes #9

--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/entity/Product.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/entity/Product.java
@@ -3,6 +3,7 @@ package com.luv2code.ecommerce.entity;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.Date;
