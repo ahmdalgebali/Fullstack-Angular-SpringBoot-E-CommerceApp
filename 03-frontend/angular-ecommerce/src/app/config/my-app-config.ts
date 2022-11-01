70d69db7e40eee7c1b459a74e7315406b062dc21
ahmdalgebali <ahmedelgebalywork@gmail.com>
2022-11-01 21:22:03 +0200

    Securing The Backend and Frontend with SSL (HTTPS) and update the needed code and OKTA configuration closes #15

--- 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
     oidc: {
         clientId: '0oa72jp4qmqBNUxDm5d7',
         issuer: 'https://dev-61477591.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 
